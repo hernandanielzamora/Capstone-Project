91bfdf145362eebb72d5fe66b7e1532a3a6bad03
Hernan Zamora <hernanzamora1002@gmail.com>
2023-03-14 21:22:29 -0300

    Fixed desing mistakes - Project is more like the template

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 const getMenuBtn = document.getElementById('ham-menu');
 const getNavBar = document.querySelector('.mobile-navbar');
+const getClosingButton = document.querySelectorAll('[data-close-button]');
 
 getMenuBtn.addEventListener('click', () => {
   getMenuBtn.classList.toggle('active');
@@ -11,6 +12,12 @@ document.querySelectorAll('.nave-link').forEach((n) => n.addEventListener('click
   getNavBar.classList.remove('active');
 }));
 
+getClosingButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    getMenuBtn.classList.remove('active');
+    getNavBar.classList.remove('active');
+  });
+});
 /* Render Speakers Cards */
 
 const speakers = [
@@ -21,7 +28,7 @@ const speakers = [
     name: 'Georgios Yannakakis',
     link: 'https://techandsoc.com/2023-conference/program/georgios-yannakakis',
     position: 'Professor and Director. Institute of digital games. University of Malta',
-    description: 'He has been involved in a number of journal editorial boards; he is currently an Associate Editor of the IEEE Transactions on Games and the IEEE Transactions on Evolutionary Computation and has been an Associate Editor of the IEEE Transactions on Affective Computing (2009-2017)',
+    description: 'Currently an Associate Editor of the IEEE Transactions on Games and the IEEE Transactions on Evolutionary Computation',
   },
   {
     grid: 2,
@@ -30,7 +37,7 @@ const speakers = [
     name: 'Vanessa Camilleri',
     link: 'https://techandsoc.com/2023-conference/program/vanessa-camilleri',
     position: 'Faculty of ICT, University of Malta, Malta',
-    description: ' Her areas of interest include Virtual Reality applications for developing emotional intelligence values. Her previous experience in the area of education and pedagogy, as well as educational technologies and the use of games for learning, have contributed to her overall academic profile.',
+    description: 'Has experience in the area of education and pedagogy, as well as educational technologies and the use of games for learning, have contributed to her overall academic profile.',
   },
   {
     grid: 1,
@@ -39,7 +46,7 @@ const speakers = [
     name: 'Alexiei Dingli',
     link: 'https://techandsoc.com/2023-conference/program/alexiei-dingli',
     position: 'Professor of AI, University of Malta, Malta',
-    description: 'He has been conducting research and working in the field of AI for more than two decades, assisting different companies to implement AI solutions. His work has been rated World Class by international experts and he won several local and international awards (such as those by the European Space Agency, the World Intellectual Property Organization and the United Nations to name a few).',
+    description: 'He has been conducting research and working in the field of AI for more than two decades, assisting different companies to implement AI solutions.',
   },
   {
     grid: 2,
@@ -47,7 +54,7 @@ const speakers = [
     img: 'media/speaker-04.jpg',
     name: 'Jaime Constenla Núñez',
     link: 'https://educacion.ucsc.cl/persona/jaime-constenla-nunez/',
-    position: 'Director Centro "INNOVAPEDIA", Profesor, Universidad Católica de la Santísima Concepción, Chile',
+    position: 'Director Centro "INNOVAPEDIA"',
     description: 'His lines of research focus on evaluative conceptions and practices, entrepreneurship and innovation in education, and pedagogical intervention in higher education.',
   },
   {
@@ -57,7 +64,7 @@ const speakers = [
     name: 'Bill Cope',
     link: 'https://ubi-learn.com/about/history/2022-conference/bill-cope',
     position: 'Professor, University of Illinois, Urbana-Champaign, USA',
-    description: 'He is also a director of Common Ground Research Networks, a not-for-profit publisher and developer of "social knowledge" technologies. He is a former First Assistant Secretary in the Department of the Prime Minister and Cabinet and Director of the Office of Multicultural Affairs. His research interests include theories and practices of pedagogy, cultural and linguistic diversity, and new technologies of representation and communication.',
+    description: 'Director of Common Ground Research Networks, a not-for-profit publisher and developer of "social knowledge" technologies. Former First Assistant Secretary in the Department of the Prime Minister.',
   },
   {
     grid: 2,
@@ -66,7 +73,7 @@ const speakers = [
     name: 'Mary Kalantzis',
     link: 'https://ubi-learn.com/about/history/2022-conference/mary-kalantzis',
     position: 'Professor, University of Illinois, Urbana-Champaign, USA',
-    description: 'Mary Kalantzis was dean of the College of Education at the University of Illinois, Urbana-Champaign, United States from 2006 to 2016. Before this, she was dean of the Faculty of Education, Language and Community Services at RMIT University, Melbourne, Australia, and president of the Australian Council of Deans of Education. With Bill Cope, she has co-authored or co-edited: New Learning: Elements of a Science of Education, Cambridge University Press, 2008 (2nd edition, 2012)',
+    description: 'Mary Kalantzis was dean of the College of Education at the University of Illinois, Urbana-Champaign, United States from 2006 to 2016.',
   },
 ];
 
@@ -76,13 +83,13 @@ const renderSpeakers = (speakers, containerName) => {
   speakers.forEach((item) => {
     const card = document.createElement('div');
     card.classList = 'speaker';
-    card.innerHTML = ` <div class="speaker-image"><img src="${item.img}" alt="${item.name}"></div>
-    <div class="speaker-name">
-      <a href="${item.link}">${item.name}</a>
-    </div>
-    <div class="speaker-position">${item.position}</div>
-    <div class="blank-space"></div>
-    <div class="speaker-text">${item.description}</div>
+    card.innerHTML = ` <div class="speaker-image"><img src="${item.img}"                  alt="${item.name}"></div>
+                        <div class="speaker-name">
+                          <a href="${item.link}">${item.name}</a>
+                          <div class="speaker-position">${item.position}</div>
+                          <div class="speaker-text">${item.description}</div>
+                        </div>
+   
   </div>`;
     containerName.appendChild(card);
   });
